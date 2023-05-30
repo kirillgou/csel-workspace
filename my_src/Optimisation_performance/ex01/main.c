0d0e859f58fcee76e0aaa715e3d9233e1b16d3dd
Tanguy Dietrich <tanguydietrich@gmail.com>
2023-05-30 18:04:03 +0200

    Working on the report

---
+++ my_src/Optimisation_performance/ex01/main.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+
+#define SIZE 5000
+
+static int32_t array[SIZE][SIZE];
+
+int main (void)
+{
+    int i, j, k;
+
+    
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            for (k = 0; k < 10; k++)
+            {
+                array[j][i]++;
+            }
+        }
+    }
+
+    // for (k = 0; k < 10; k++)
+    // {
+    //     for (i = 0; i < SIZE; i++)
+    //     {
+    //         for (j = 0; j < SIZE; j++)
+    //         {
+    //             array[j][i]++;
+    //         }
+    //     }
+    // }
+    return 0;
+}
+
