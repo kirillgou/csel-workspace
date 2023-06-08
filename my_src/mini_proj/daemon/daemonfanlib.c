dcc92acf756c71738fce18f5c2d31623bbeadf67
Tanguy Dietrich <tanguydietrich@gmail.com>
2023-06-08 20:41:02 +0200

    daemon running, button and writing on /sys/class + write on screen ok

--- my_src/mini_proj/daemon/daemonfanlib.c
+++ my_src/mini_proj/daemon/daemonfanlib.c
@@ -10,35 +10,42 @@ S2 pour diminuer la vitesse de rotation du ventilateur, la pression du S2 devra 
 S3 pour changer du mode automatique au mode manuel et vice versa.
 Interface IPC, au choix du développeur, permettant de dialoguer avec une application pour choisir le mode de fonctionnement et spécifier la fréquence de clignotement
 Le daemon utilisera l’écran OLED pour indiquer le mode de fonctionnement, la température actuelle du microprocesseur ainsi que la fréquence de clignotement de la LED Status.
-
 Une application fournira une interface utilisateur, une ligne de commande, pour piloter le système via l’interface IPC choisie.
 
-nous avons decider d'utiliser un soket unix pour la communication entre le daemon et l'application
+Remarque :
+nous avons decider d'utiliser un soket unix pour la communication entre le daemon et l'application.
+Nous assumons qu'il n'y auras qu'une seul connection sur le socket.
 */
 
-*/
 #define _XOPEN_SOURCE 600
 #define _DEFAULT_SOURCE
 
-#include <fcntl.h>
-#include <grp.h>
-#include <pwd.h>
-#include <signal.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <syslog.h>
-#include <unistd.h>
-
 #include "daemonfanlib.h"
+
+static void catch_signal(int signal);
+static void *threadSocket(void *arg);
+static void initSocket();
+static void initButtons();
+static void initLeds();
+static void initScreen();
+static void daemonFunc();
+static void fork_process();
+static void writeMode(int mode);
+static void writeFreq(int freq);
 
 
 // GLOBAL VARIABLES
 static int g_signal_catched = 0;
 static int g_server_fd = 0;
-struct sockaddr_un g_address;
+static int g_client_fd = 0;
+static int g_led_fd = 0;
+static int g_mode = 0;
+static int g_freq = 0;
+static struct sockaddr_in g_address;
+static struct sockaddr_in g_clientaddress;
+static pthread_t g_thread_id;
+static my_context g_ctx[3];
+
 
 static void catch_signal(int signal)
 {
@@ -46,33 +53,69 @@ static void catch_signal(int signal)
     g_signal_catched++;
 }
 
-void *threadWaitConnection(void *arg)
+static void *threadSocket(void *arg)
 {
-    // wait for a new connection on the socket
-    int new_socket;
-    struct sockaddr clientaddress;
+    // TODO : ADD A SIGNAL TO STOP THE THREAD
+    int addresslen = sizeof(g_clientaddress);
+    //listen on the socket
     while(1) {
-        if((new_socket = accept(g_server_fd, (struct sockaddr*)&clientaddress, sizeof(clientaddress))) < 0) {
-            syslog(LOG_ERR, "accept");
-            exit(EXIT_FAILURE);
+        char buffer[1024] = {0};
+        int valread = read(g_client_fd, buffer, 1024);
+        if (valread == 0) {
+            syslog(LOG_INFO, "client disconnected\n");
+            close(g_client_fd);
+            g_client_fd = accept(g_server_fd, (struct sockaddr*)&g_clientaddress, ((socklen_t*) &addresslen));
+        } else {
+            syslog(LOG_INFO, "received: %s\n", buffer);
         }
-        syslog(LOG_INFO, "new connection accepted\n");
-        // launch a thread to process the communication with the application
-        pthread_t threadProcessMessage;
-        pthread_create(&threadProcessMessage, NULL, processMessage, NULL);
-        // TODO : GET RID OF THE join
-        pthread_join(threadProcessMessage, NULL);
     }
+    return NULL;
 }
 
-void *processMessage(void *arg)
+static void writeMode(int mode)
 {
-    while
+    char str[32] = {0};
+    // open MODULE_FILE_MODE in write mode
+    int fd = open(MODULE_FILE_MODE, O_WRONLY);
+    if (fd < 0) {
+        syslog(LOG_ERR, "open %s failed\n", MODULE_FILE_MODE);
+        exit(EXIT_FAILURE);
+    }
+    // write mode in the file
+    char buffer[2] = {0};
+    sprintf(buffer, "%d", mode);
+    write(fd, buffer, strlen(buffer));
+    close(fd);
+    // add a line for the mode
+    sprintf(str, "Mode: %d", mode);
+    ssd1306_set_position (0,6);
+    ssd1306_puts(str);
 }
 
-void initsocket()
+static void writeFreq(int freq)
+{
+    char str[32] = {0};
+    int fd = open(MODULE_FILE_MODE, O_WRONLY);
+    if (fd < 0) {
+        syslog(LOG_ERR, "open %s failed\n", MODULE_FILE_MODE);
+        exit(EXIT_FAILURE);
+    }
+    // write mode in the file
+    char buffer[2] = {0};
+    sprintf(buffer, "%d", freq);
+    // TODO : check if write is ok
+    write(fd, buffer, strlen(buffer));
+    close(fd);
+    // then write new data to the screen
+    sprintf(str, "Freq: %dHz", freq);
+    ssd1306_set_position (0 ,4);
+    ssd1306_puts(str);
+}
+
+static void initSocket()
 {
     int opt = 1;
+    int adddresslen = sizeof(g_address);
     
     // init a socket for communication with the application
     if ((g_server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -91,19 +134,95 @@ void initsocket()
         syslog(LOG_ERR, "bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(g_server_fd, 3) < 0) {
+    if (listen(g_server_fd, 1) < 0) {
         syslog(LOG_ERR, "listen");
         exit(EXIT_FAILURE);
     }
 
-    // launch a thread to wait for a new connection
-    pthread_t threadWaitMessage;
-    pthread_create(&threadWaitMessage, NULL, threadWaitMessage, NULL);
+    if((g_client_fd = accept(g_server_fd, (struct sockaddr*)&g_clientaddress, ((socklen_t*) &adddresslen))) < 0) {
+        syslog(LOG_ERR, "accept");
+        exit(EXIT_FAILURE);
+    }
 
+    // TODO : make a join on the thread
+    pthread_create(&g_thread_id, NULL, threadSocket, NULL);
+    syslog(LOG_INFO, "socket initialized\n");
+}
+
+int open_button(const char *gpio_path, const char *gpio_num){
+    // printf("open button %s %s\n", gpio_num, gpio_path);
+    // unexport pin out of sysfs (reinitialization)
+    int f = open(GPIO_UNEXPORT, O_WRONLY);
+    write(f, gpio_num, strlen(gpio_num));
+    close(f);
+
+    // export pin to sysfs
+    f = open(GPIO_EXPORT, O_WRONLY);
+    write(f, gpio_num, strlen(gpio_num));
+    close(f);
+
+    // config pin
+    char *path = malloc(strlen(gpio_path) + strlen("/direction") + 1);
+    strcpy(path, gpio_path);
+    strcat(path, "/direction");
+    f = open(path, O_WRONLY);
+    write(f, "in", 2);
+    close(f);
+    free(path);
+
+    // config edge
+    path = malloc(strlen(gpio_path) + strlen("/edge") + 1);
+    strcpy(path, gpio_path);
+    strcat(path, "/edge");
+    f = open(path, O_WRONLY);
+    write(f, "rising", 6);
+    // write(f, "falling", 7);
+    // write(f, "both", 4);
+    close(f);
+    free(path);
+
+    // open gpio value attribute
+    path = malloc(strlen(gpio_path) + strlen("/value") + 1);
+    strcpy(path, gpio_path);
+    strcat(path, "/value");
+    f = open(path, O_RDWR);
+    free(path);
+    return f;
+}
+
+static void initButtons()
+{
+    g_ctx[EV_BTN_1].ev = EV_BTN_1;
+    g_ctx[EV_BTN_2].ev = EV_BTN_2;
+    g_ctx[EV_BTN_3].ev = EV_BTN_3;
+    g_ctx[EV_BTN_1].fd = open_button(GPIO_BTN_1, BTN_1);
+    g_ctx[EV_BTN_2].fd = open_button(GPIO_BTN_2, BTN_2);
+    g_ctx[EV_BTN_3].fd = open_button(GPIO_BTN_3, BTN_3);
+    syslog(LOG_INFO, "buttons initialized\n");
 
 }
 
-void initScreen()
+static void initLeds()
+{
+    int f = open(GPIO_UNEXPORT, O_WRONLY);
+    write(f, LED, strlen(LED));
+    close(f);
+
+    // export pin to sysfs
+    f = open(GPIO_EXPORT, O_WRONLY);
+    write(f, LED, strlen(LED));
+    close(f);
+
+    // config pin
+    f = open(GPIO_LED "/direction", O_WRONLY);
+    write(f, "out", 3);
+    close(f);
+    g_led_fd = open(GPIO_LED "/value", O_WRONLY);
+    syslog(LOG_INFO, "leds initialized\n");
+}
+
+
+static void initScreen()
 {
     ssd1306_init();
 
@@ -120,34 +239,108 @@ void initScreen()
     ssd1306_puts("Freq: 1Hz");
     ssd1306_set_position (0,5);
     ssd1306_puts("Duty: 50%");
+    syslog(LOG_INFO, "screen initialized\n");
 }
 
 // this will be called when in daemon mode
-void daemonFunc()
+static void daemonFunc()
 {
-    int server_fd, new_socket, valread;
-    int opt = 1;
-    struct sockaddr_un address;
+    syslog(LOG_INFO, "daemon function\n");
     // start by initialising the ssd1306 display
-   initScreen();
+    initScreen();
 
     // init a socket for communication with the application
-    
+    // initSocket();
+
     // init the buttons S1, S2 and S3
+    initButtons();
 
+    int epfd = epoll_create1(0);// parametre size isn't used anymore by Linux
 
-    // main loop
-    // while (signal_catched < 3) {
-    // }
-    // passive wait for signals
-    // temporary solution
-    while (1) {
-        sleep(1);
+    for(uint32_t i = 0; i < 3; i++){
+        g_ctx[i].first_done = 0;
+        struct epoll_event event = {
+            // .events = EPOLLIN | EPOLLET, 
+            .events = EPOLLET, // mode edge triggered
+            .data.ptr = &g_ctx[i]
+        };
+        if (epoll_ctl(epfd, EPOLL_CTL_ADD, g_ctx[i].fd, &event) == -1) {
+            printf("error epoll_ctl: %s\n", strerror(errno));
+            // return 1;
+            exit(EXIT_FAILURE);
+        }
     }
+    // creation contexte epoll
+    if (epfd == -1) {
+        // printf("error epoll_create1: %s\n", strerror(errno));
+        syslog(LOG_ERR, "epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+
+    // init the leds
+    initLeds();
+
+    
+
+    while (1) {
+        struct epoll_event event_arrived[3];
+        syslog(LOG_INFO, "waiting for event epoll\n");
+        int nr = epoll_wait(epfd, event_arrived, 3, -1);
+        syslog(LOG_INFO, "event arrived\n");
+        if (nr == -1) {
+            // printf("error epoll_wait: %s\n", strerror(errno));
+            syslog(LOG_ERR, "epoll_wait");
+            exit(EXIT_FAILURE);
+        }
+        for(int i = 0; i < nr; i++){
+            my_context *ctx = event_arrived[i].data.ptr;
+
+            switch (ctx->ev){
+            case EV_BTN_1: // increase frequence
+            syslog(LOG_INFO, "button 1 pressed\n");
+                if(ctx->first_done == 0){
+                    ctx->first_done = 1;
+                    break;
+                }
+                g_freq++;
+                writeFreq(g_freq);
+            break;
+            case EV_BTN_2: // decrease frequence
+            syslog(LOG_INFO, "button 2 pressed\n");
+            if(ctx->first_done == 0){
+                    ctx->first_done = 1;
+                    break;
+                }
+                g_freq--;
+                writeFreq(g_freq);
+            break;
+            case EV_BTN_3: // change auto/manual mode
+            syslog(LOG_INFO, "button 3 pressed\n");
+                if(ctx->first_done == 0){
+                    ctx->first_done = 1;
+                    break;
+                }
+                // button_action(ctx->ev, 0);
+                g_mode = !g_mode;
+                //TODO : write it to the the module
+                writeMode(g_mode);
+                break;
+            
+            default:
+                syslog(LOG_ERR, "unknow event");
+                // printf("error: unknow event\n");
+                break;
+            }
+        }
+    }
+    // TODO : SEND A SIGNAL TO KILL THE LISTENING THREAD
+    kill(g_thread_id, SIGKILL);
+    pthread_join(g_thread_id, NULL);
 }
 
 void generateDaemon()
 {
+
     // 1. fork off the parent process
     fork_process();
 
@@ -201,32 +394,33 @@ void generateDaemon()
 
     // 9. option: open syslog for message logging
     openlog(NULL, LOG_NDELAY | LOG_PID, LOG_DAEMON);
-    syslog(LOG_INFO, "Daemon has started...");
+    syslog(LOG_INFO, "Daemon has started 7...");
 
-    // 10. option: get effective user and group id for appropriate's one
-    struct passwd* pwd = getpwnam("daemon");
-    if (pwd == 0) {
-        syslog(LOG_ERR, "ERROR while reading daemon password file entry");
-        exit(1);
-    }
+    // // 10. option: get effective user and group id for appropriate's one
+    // struct passwd* pwd = getpwnam("daemon");
+    // if (pwd == 0) {
+    //     syslog(LOG_ERR, "ERROR while reading daemon password file entry");
+    //     exit(1);
+    // }
 
-    // 11. option: change root directory
-    if (chroot(".") == -1) {
-        syslog(LOG_ERR, "ERROR while changing to new root directory");
-        exit(1);
-    }
+    // // 11. option: change root directory
+    // if (chroot(".") == -1) {
+    //     syslog(LOG_ERR, "ERROR while changing to new root directory");
+    //     exit(1);
+    // }
 
-    // 12. option: change effective user and group id for appropriate's one
-    if (setegid(pwd->pw_gid) == -1) {
-        syslog(LOG_ERR, "ERROR while setting new effective group id");
-        exit(1);
-    }
-    if (seteuid(pwd->pw_uid) == -1) {
-        syslog(LOG_ERR, "ERROR while setting new effective user id");
-        exit(1);
-    }
+    // // 12. option: change effective user and group id for appropriate's one
+    // if (setegid(pwd->pw_gid) == -1) {
+    //     syslog(LOG_ERR, "ERROR while setting new effective group id");
+    //     exit(1);
+    // }
+    // if (seteuid(pwd->pw_uid) == -1) {
+    //     syslog(LOG_ERR, "ERROR while setting new effective user id");
+    //     exit(1);
+    // }
 
     // 13. implement daemon body...
+    syslog(LOG_INFO, "running daemon");
     daemonFunc();
 
     syslog(LOG_INFO, "daemon stopped. Number of signals catched=%d\n", g_signal_catched);
--- my_src/mini_proj/daemon/daemonfanlib.h
+++ my_src/mini_proj/daemon/daemonfanlib.h
@@ -14,11 +14,45 @@
 #include <fcntl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/epoll.h>
 
+#include "ssd1306.h"
+
+
+#define MODULE_FILE_MODE "/sys/class/fan_ctl/fan_ctl/auto_config"
+#define MODULE_FILE_SPEED "/sys/class/fan_ctl/fan_ctl/frequency_Hz"
+#define MODULE_FILE_TEMP "/sys/class/fan_ctl/fan_ctl/temperature_mC"
 #define DAEMON_PORT 8080
 
-static void fork_process();
-static void catch_signal(int signal);
+#define GPIO_EXPORT   "/sys/class/gpio/export"
+#define GPIO_UNEXPORT "/sys/class/gpio/unexport"
+
+#define GPIO_LED      "/sys/class/gpio/gpio10"
+#define LED           "10"
+
+#define GPIO_BTN_1   "/sys/class/gpio/gpio0"
+#define BTN_1        "0"
+#define GPIO_BTN_2   "/sys/class/gpio/gpio2"
+#define BTN_2        "2"
+#define GPIO_BTN_3   "/sys/class/gpio/gpio3"
+#define BTN_3        "3"
+
+enum my_event {
+    EV_BTN_1 = 0,
+    EV_BTN_2 = 1,
+    EV_BTN_3 = 2
+};
+
+typedef struct {
+    int fd;
+    enum my_event ev;
+    int first_done;
+} my_context;
+
+void generateDaemon();
 
 
 #endif
